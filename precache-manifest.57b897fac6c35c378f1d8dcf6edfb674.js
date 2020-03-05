c265831d9e142aeee25b3fcfdcd428ecd309672f
windchime-yk <windchime-yk@users.noreply.github.com>
2020-03-05 14:18:24 +0000

    Automated deployment: Thu Mar  5 14:18:24 UTC 2020 43535dde3d9f46ccd11182c0b63256f6080cd198

---
+++ precache-manifest.57b897fac6c35c378f1d8dcf6edfb674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46760cdfa7e2b5ffaffa1e64bd8e40c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b3a0d0c48e16e86bbd",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "037bf15768c3413b2ec9",
+    "url": "/static/js/2.4bfbdadf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4bfbdadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b3a0d0c48e16e86bbd",
+    "url": "/static/js/main.5f0ee929.chunk.js"
+  },
+  {
+    "revision": "0d1ef1cbb6be274a781c",
+    "url": "/static/js/runtime-main.36263dc2.js"
+  }
+]);
